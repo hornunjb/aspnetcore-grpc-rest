3bbe976b7a22d91eaaa97de60f1b1d4720b815a2
Ben Chen <benjamin_chen@outlook.com>
2020-12-17 23:27:21 -0800

    More clean up

--- aspnetapp/Startup.cs
+++ aspnetapp/Startup.cs
@@ -112,10 +112,7 @@ namespace aspnetapp
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
             // specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-            });
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
 
             app.UseRouting();
 
